ae9c7bef9caee3f0ff8b690ef53c8de177b5af8c
Tim te Beek <tim.te.beek@jdriven.com>
2019-10-06 16:25:15 +0200

    Suffix remote service name "kafka" with topic name for dependency graph

--- src/main/java/com/github/timtebeek/KafkaStreamsLogCorrelationApplication.java
+++ src/main/java/com/github/timtebeek/KafkaStreamsLogCorrelationApplication.java
@@ -1,7 +1,11 @@
 package com.github.timtebeek;
 
+import brave.handler.FinishedSpanHandler;
+import brave.handler.MutableSpan;
+import brave.propagation.TraceContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
@@ -12,4 +16,18 @@ public class KafkaStreamsLogCorrelationApplication {
 		SpringApplication.run(KafkaStreamsLogCorrelationApplication.class, args);
 	}
 
+	@Bean
+	public FinishedSpanHandler finishedSpanHandler() {
+		// Suffix remote service name "kafka" with topic name for clearer dependency graph
+		return new FinishedSpanHandler() {
+			@Override
+			public boolean handle(TraceContext context, MutableSpan span) {
+				String topic = span.tag("kafka.topic");
+				if (topic != null) {
+					span.remoteServiceName(span.remoteServiceName() + '/' + topic);
+				}
+				return true;
+			}
+		};
+	}
 }
